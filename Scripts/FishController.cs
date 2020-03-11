90871bded221d1d27ef5577d58b69b6b2b960f23
CyberNoble <l.a.edelmore@gmail.com>
2020-03-11 20:47:37 +0700

    v 0.1
    Riba Fon pochti dvijenie

---
+++ Scripts/FishController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FishController : MonoBehaviour
+{
+    public float swimForce;
+
+    private Rigidbody2D rb;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update()
+    {
+       rb.
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
