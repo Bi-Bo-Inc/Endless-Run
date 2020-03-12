12ea9b13c1a0c132636aace854752a22f92bbcd9
hannyS1 <59133695+hannyS1@users.noreply.github.com>
2020-03-12 20:31:45 +0700

    version 0.0.1

---
+++ Scripts/SpawnEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public Transform SpawnPos;
+    public GameObject Enemy;
+    private float time;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnerEnemy());
+    }
+
+    void Repeat()
+    {
+        StartCoroutine(SpawnerEnemy());
+    }
+
+    IEnumerator SpawnerEnemy()
+    {
+        time = Random.Range(1f, 5f);
+        yield return new WaitForSeconds(time);
+        Instantiate(Enemy, SpawnPos.position, Quaternion.identity);
+        Repeat();
+    }
+
+   
+   
+}
