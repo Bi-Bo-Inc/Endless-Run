fdeb978348b8c2fe236682212be82ba1eb63479e
Britvaruk <Ford_cool@mail.ru>
2020-03-12 20:21:38 +0700

    igra

---
+++ Scripts/MoveEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    private float speed;
+ 
+    public Vector2 move;
+    void Start()
+    {
+        speed = Random.Range(0.09f, 0.3f);
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        transform.Translate(move * speed);
+        if(gameObject.transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Scripts/TestFishController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFishController : MonoBehaviour
+{
+    private float swimForce = 0;
+
+    private Rigidbody2D rb;
+
+    private float acceleration = 0.3f; // ускорение
+    private float maxSwimForce = 6.5f; // максимальная скорость
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    
+
+    private void FixedUpdate()
+    {
+
+        Movement(swimForce);
+
+        //  ниже изменение ускорения
+        if (Input.touchCount > 0 && ((Input.GetTouch(0).phase == TouchPhase.Moved) || (Input.GetTouch(0).phase == TouchPhase.Stationary)|| (Input.GetTouch(0).phase == TouchPhase.Began)))
+        {
+
+
+            if (swimForce < maxSwimForce) 
+                swimForce += acceleration;  
+        }
+        else 
+        {
+            
+            
+            if (swimForce > -maxSwimForce)
+                swimForce -= acceleration;
+            
+        }
+    }
+    private void Movement(float swimForce)
+    {
+        
+        rb.velocity = new Vector2(0, swimForce);
+        
+    }
+
+    
+    
+}
