6858120902d837124249f6685652a2df5bd44517
Britvaruk <Ford_cool@mail.ru>
2020-03-13 20:11:20 +0700

    v000000

---
+++ Scripts/MoveEnemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MoveEnemy : MonoBehaviour
+{
+    public float speed; 
+    public Vector2 move;
+    
+    void FixedUpdate()
+    {
+        EnemyMoving();
+    }
+
+    void EnemyMoving()
+    {
+        transform.Translate(move * speed);
+        if (gameObject.transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
