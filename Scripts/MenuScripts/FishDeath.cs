a7be8e80f29ec3cd34943ab25af126229803711d
Britvaruk <Ford_cool@mail.ru>
2020-03-14 21:03:33 +0700

    v1.101

--- Scripts/MenuScripts/FishDeath.cs
+++ Scripts/MenuScripts/FishDeath.cs
@@ -10,11 +10,14 @@ public class FishDeath : MonoBehaviour
     public GameObject spawnEnemy, ScoreManager;
     private SpawnEnemy SE;
     private Rigidbody2D rb;
+    
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         SE = spawnEnemy.GetComponent<SpawnEnemy>();
+        //FishPlayer = GetComponent<Animation>();
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -33,25 +36,38 @@ public class FishDeath : MonoBehaviour
             ScoreScript.scoreIncreasing = false;
             GetComponent<PolygonCollider2D>().enabled = false;
 
-
-
-            //Time.timeScale = 0f;
             IsDeath = true;
 
-            transform.localScale = new Vector2(0.8527528f, -0.9311681f);
+            
         }
     }
 
+    private float fishRotation = 0; //начальное положение
+    private float fishAcceleration = 2f; //поворот
+    private float maxFishRotation = 180f;
     private void FixedUpdate()
     {
         if (IsDeath)
         {
-            rb.velocity = new Vector2(0, 2);
-            if (gameObject.transform.position.y > 10)
-            {
-                Destroy(gameObject);
-            }
+            GetComponent<Animator>().enabled = false;
+            FloatingUp();
+        }
+    }
 
+    private void FloatingUp()
+    {
+        //transform.localScale = new Vector2(0.8527528f, -0.9311681f);
+
+        if ((fishRotation >= 0) && (fishRotation < maxFishRotation))
+        {
+            transform.rotation = Quaternion.Euler(0, 0, fishRotation);
+            fishRotation += fishAcceleration;
+        }
+
+        rb.velocity = new Vector2(0, 2);
+        if (gameObject.transform.position.y > 10)
+        {
+            Destroy(gameObject);
         }
     }
 }
