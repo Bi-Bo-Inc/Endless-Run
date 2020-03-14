73efaeaf9845212d8244ed5b73f3028b4d3b3f6a
CyberNoble <l.a.edelmore@gmail.com>
2020-03-14 10:45:24 +0700

    фыв

--- Scripts/EnemyScripts/SinMoveEnemy.cs
+++ Scripts/EnemyScripts/SinMoveEnemy.cs
@@ -10,22 +10,27 @@ public class SinMoveEnemy : MonoBehaviour
 
 	[SerializeField]
 	private float magnitude = 0.5f;
+	public float minRandAmplit;
+	public float maxRandAmplit;
 
 	Vector3 pos;
 
 	void Start()
 	{
 		pos = transform.position;
+		minRandAmplit = Random.Range(0.1f, 0.4f);
 	}
 
 	void FixedUpdate()
 	{
 		MoveLeft();
+		
 	}
 
 	void MoveLeft()
 	{
+		
 		pos -= transform.right * Time.deltaTime * moveSpeed;
-		transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
+		transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency  * (magnitude+ minRandAmplit));
 	}
 }
