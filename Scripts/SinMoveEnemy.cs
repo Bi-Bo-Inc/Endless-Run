30120ac3eff9080920f5e513b4df83b1cc327e7d
CyberNoble <l.a.edelmore@gmail.com>
2020-03-14 11:34:20 +0700

    Revert "фыв"
    This reverts commit aa99f5204a0694ad663d49806e902438ba8f48ce.

--- Scripts/SinMoveEnemy.cs
+++ Scripts/SinMoveEnemy.cs
@@ -10,32 +10,22 @@ public class SinMoveEnemy : MonoBehaviour
 
 	[SerializeField]
 	private float magnitude = 0.5f;
-	public float minRandAmplit;
-	public float maxRandAmplit;
-	private float randForMagn;
 
 	Vector3 pos;
 
 	void Start()
 	{
 		pos = transform.position;
-		randForMagn = Random.Range(minRandAmplit, maxRandAmplit);
 	}
 
 	void FixedUpdate()
 	{
 		MoveLeft();
-		
 	}
 
 	void MoveLeft()
 	{
-		
 		pos -= transform.right * Time.deltaTime * moveSpeed;
-		transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency  * (magnitude + randForMagn));
-		if (gameObject.transform.position.x < -15)
-		{
-			Destroy(gameObject);
-		}
+		transform.position = pos + transform.up * Mathf.Sin(Time.time * frequency) * magnitude;
 	}
 }
