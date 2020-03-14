e9622ec5592ca9c4afaf389597bf813780b4bf41
hannyS1 <59133695+hannyS1@users.noreply.github.com>
2020-03-14 16:37:52 +0700

    1.01

--- Scripts/MenuScripts/FishDeath.cs
+++ Scripts/MenuScripts/FishDeath.cs
@@ -6,6 +6,16 @@ public class FishDeath : MonoBehaviour
     public GameObject pauseMenuUI, scoreUI, scoreValueUI, pauseButtonUI, reloadButtonUI, highScoreUI, highScoreValueUI;    
 
     public static bool IsDeath = false;
+  
+    public GameObject spawnEnemy, ScoreManager;
+    private SpawnEnemy SE;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        SE = spawnEnemy.GetComponent<SpawnEnemy>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -19,10 +29,29 @@ public class FishDeath : MonoBehaviour
             pauseButtonUI.SetActive(false);
             reloadButtonUI.SetActive(false);
 
-            Time.timeScale = 0f;
+            SE.IsSpawner(false);
+            ScoreScript.scoreIncreasing = false;
+            GetComponent<PolygonCollider2D>().enabled = false;
+
+
+
+            //Time.timeScale = 0f;
             IsDeath = true;
 
             transform.localScale = new Vector2(0.8527528f, -0.9311681f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsDeath)
+        {
+            rb.velocity = new Vector2(0, 2);
+            if (gameObject.transform.position.y > 10)
+            {
+                Destroy(gameObject);
+            }
+
         }
     }
 }
