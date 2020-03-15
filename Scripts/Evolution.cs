e71bafbc213a6a14c4792dd46b32231c0c7af443
hannyS1 <59133695+hannyS1@users.noreply.github.com>
2020-03-15 17:43:34 +0700

    1.45

--- Scripts/Evolution.cs
+++ Scripts/Evolution.cs
@@ -3,10 +3,21 @@ using UnityEngine;
 
 public class Evolution : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public GameObject scoreValue;
     int scoreForEvol = 0;    
     public int scoreGoal1;
     public int scoreGoal2;
+=======
+
+    public GameObject PlayerFish2, PlayerFish;
+    public GameObject scoreValue;
+    int scoreForEvol = 0;
+    int codeForEvolve = -1;
+    public int scoreGoal;
+    
+    
+>>>>>>> Stashed changes
     //public int scoreGoal3;
     private void Start()
     {
@@ -15,6 +26,7 @@ public class Evolution : MonoBehaviour
     private void FixedUpdate()
     {
         scoreForEvol = ScoreScript.scoreValue;
+<<<<<<< Updated upstream
         if (scoreForEvol == scoreGoal1)
             Evolve(2);
         else if (scoreForEvol == scoreGoal2)
@@ -22,6 +34,17 @@ public class Evolution : MonoBehaviour
     }
 
     private void Evolve(int codeForEvolve)
+=======
+        if (scoreForEvol == scoreGoal)
+        {
+           
+            PlayerFish2.SetActive(true);
+            PlayerFish.SetActive(false);
+        }
+
+    }
+    private void Evolve(int codeForeEvolve)
+>>>>>>> Stashed changes
     {
         SceneManager.LoadScene(codeForEvolve);
     }
