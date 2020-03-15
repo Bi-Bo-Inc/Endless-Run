aa276ba55a1309274aedde0a0ae08cf2b4779cff
CyberNoble <l.a.edelmore@gmail.com>
2020-03-15 17:48:27 +0700

    a

--- Scripts/Evolution.cs
+++ Scripts/Evolution.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 public class Evolution : MonoBehaviour
 {
     public GameObject scoreValue;
-    int scoreForEvol = 0;
-    int codeForEvolve = -1;
+    int scoreForEvol = 0;    
     public int scoreGoal1;
     public int scoreGoal2;
     //public int scoreGoal3;
@@ -22,7 +21,7 @@ public class Evolution : MonoBehaviour
             Evolve(3);
     }
 
-    private void Evolve(int codeForeEvolve)
+    private void Evolve(int codeForEvolve)
     {
         SceneManager.LoadScene(codeForEvolve);
     }
