0e06b0cdd92c2691c2eddf721e8edaae63f70991
CyberNoble <55918004+CyberNoble@users.noreply.github.com>
2020-03-15 17:41:12 +0700

    Add files via upload

---
+++ MoveEntity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class MoveEntity : MonoBehaviour
+{
+    public float Entityspeed;
+    public Vector2 moveEntity;
+
+    void FixedUpdate()
+    {
+        EntityMoving();
+    }
+
+    void EntityMoving()
+    {
+        transform.Translate(Entityspeed * moveEntity);
+        if (gameObject.transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
