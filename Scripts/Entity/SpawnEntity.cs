9bd526302f669bd0d18542b4c40b0c2808178d2f
CyberNoble <l.a.edelmore@gmail.com>
2020-03-16 16:35:22 +0700

    w

--- Scripts/Entity/SpawnEntity.cs
+++ Scripts/Entity/SpawnEntity.cs
@@ -3,15 +3,16 @@ using UnityEngine;
 
 public class SpawnEntity : MonoBehaviour
 {
-    public GameObject vodorosl0, vodorosl1;//, vodorosl2, vodorosl3, vodorosl4, vodorosl5, vodorosl6, vodorosl7, vodorosl8, vodorosl9;
+    public GameObject bubble,vodorosl0, vodorosl1;//, vodorosl2, vodorosl3, vodorosl4, vodorosl5, vodorosl6, vodorosl7, vodorosl8, vodorosl9;
     public float minTime, maxTime;
     private GameObject currentVodorosl;
     private void Start()
     {
         StartCoroutine(SpawnerEntityVodorosl());
+        StartCoroutine(SpawnerEntityBubble());
     }
 
-    private void Spawn(GameObject curVodorosl)
+    private void SpawnVodorosl(GameObject curVodorosl)
     {
         GameObject vodorosl = curVodorosl;
         Instantiate(vodorosl, new Vector2(15f, -4f), Quaternion.identity);
@@ -23,11 +24,11 @@ public class SpawnEntity : MonoBehaviour
         {
             case 0:
                 currentVodorosl = vodorosl0;
-                Spawn(currentVodorosl);
+                SpawnVodorosl(currentVodorosl);
                 break;
             case 1:
                 currentVodorosl = vodorosl1;
-                Spawn(currentVodorosl);
+                SpawnVodorosl(currentVodorosl);
                 break;
             default:
                 break;
@@ -44,6 +45,22 @@ public class SpawnEntity : MonoBehaviour
             yield return new WaitForSeconds(time);
             ChooseVodorosl(randVodorosl);
 
+        }
+    }
+
+    private void SpawnBubble()
+    {
+        float x = Random.Range(-9f, 9f);
+        Instantiate(bubble, new Vector2(x,-5.8f),Quaternion.identity);
+    }
+
+    IEnumerator SpawnerEntityBubble()
+    {
+        while (true)
+        {
+            float time = Random.Range(0, 0.75f);
+            yield return new WaitForSeconds(time);
+            SpawnBubble();
         }
     }
 }
