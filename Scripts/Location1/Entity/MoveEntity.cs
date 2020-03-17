54c9617e0c476c5c95e9408054b5d8763d17290f
hannyS1 <59133695+hannyS1@users.noreply.github.com>
2020-03-17 11:47:26 +0700

    1.56
    fixed bag with 1st fish

---
+++ Scripts/Location1/Entity/MoveEntity.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MoveEntity : MonoBehaviour
+{
+    public float Entityspeed;
+    public Vector2 moveEntity;
+
+    void FixedUpdate()
+    {
+        VodoroslMoving();
+       
+    }
+
+    void VodoroslMoving()
+    {
+        transform.Translate(Entityspeed * moveEntity);
+        if (gameObject.transform.position.x < -15)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+   
+}
