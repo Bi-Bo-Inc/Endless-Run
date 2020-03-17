0479c4ece48c3bfcbb2614e7384baaa7c4a8e82e
CyberNoble <l.a.edelmore@gmail.com>
2020-03-17 13:27:45 +0700

    в

---
+++ Scripts/Location1/BackgroundMoving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMoving : MonoBehaviour
+{
+    public float speed;
+    public Vector2 vector;
+
+    void FixedUpdate()
+    {
+        BackgroundMove();
+    }
+
+    void BackgroundMove()
+    {
+        transform.Translate(vector * speed);
+        if (gameObject.transform.position.x < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
