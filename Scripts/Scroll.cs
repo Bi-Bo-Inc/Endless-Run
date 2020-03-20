9a1f9b90e19f7ac209e6412a03e59a7db5172030
Britvaruk <Ford_cool@mail.ru>
2020-03-20 21:52:46 +0700

    v1.7

---
+++ Scripts/Scroll.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 0;
+
+    
+    public Transform neighbour;
+
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        transform.Translate(Vector2.left * speed * Time.smoothDeltaTime);
+        if (gameObject.transform.position.x < -30)
+        {
+            SnapToNeighbour();
+        }
+    }
+
+    public void SnapToNeighbour()
+    {
+        transform.position = new Vector2(neighbour.position.x, transform.position.y);    
+    }
+}
+
+
+
