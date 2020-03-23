e0e70f21eab2c0dd18492f8316197d18e6a4e36d
CyberNoble <l.a.edelmore@gmail.com>
2020-03-24 00:26:54 +0700

    1

---
+++ Scripts/Testing/Test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    //[SerializeField]
+    //private GameObject[] enemyFish = new GameObject[mapLength];
+
+    //public static bool isSpawnerOn = true;
+
+    //private void Start()
+    //{
+    //    for (int i = 0;i<)
+    //}
+}
